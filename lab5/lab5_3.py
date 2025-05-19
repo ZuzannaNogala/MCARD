476e815d6ec539ae97d9dbb373d086d4961085af
ZuzannaNogala <zuzanna02nogala@gmail.com>
2025-05-19 18:08:50 +0200

    clear the code

--- lab5/lab5_3.py
+++ lab5/lab5_3.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -11,7 +10,7 @@ from sklearn.metrics import accuracy_score
 import time
 import numpy as np
 
-from lab5.lab5_models import categorical_crossentropy
+from lab5.lab5_models import categorical_crossentropy, MyTorchClassifierTrainer
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -40,11 +39,31 @@ y_test_tensor = torch.tensor(y_test, dtype=torch.long)
 y_train_tensor = F.one_hot(y_train_tensor, num_classes=3).float()
 y_test_tensor = F.one_hot(y_test_tensor, num_classes=3).float()
 
+# Functions:
+
+
+def create_dataloader(dataset, batch_size):
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return dataloader
+
+
+def accuracy_and_predictions_model(model, x_test_tensor=X_test_tensor, Y_test=y_test):
+    model.eval()
+    with torch.no_grad():
+        test_outputs = model(x_test_tensor.to(device))
+        # Get predicted classes by taking argmax
+        torch_preds = test_outputs.argmax(dim=1).cpu().numpy()
+        print(f"predictions of y_test: {torch_preds}")
+
+    # Accuracy:
+    print("accuracy  = \t\t", accuracy_score(torch_preds, Y_test),
+          "correctly cl. cases=", np.sum(torch_preds == Y_test), " out of ", len(Y_test))
+
 
 # Model 1 - training with batch_size = 128
-batch_size = 128
+
 train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+train_loader = create_dataloader(train_dataset, batch_size=128)
 
 
 WineClassifer_torch_model = nn.Sequential(
@@ -55,46 +74,23 @@ WineClassifer_torch_model = nn.Sequential(
     nn.Linear(5, num_classes),
     nn.Softmax(dim=1)
 ).to(device)
+print(" ")
+print("MODEL 1 (batch_size=128)")
 
-# Setup optimizer
-optimizer = optim.Adam(WineClassifer_torch_model.parameters(), lr=0.01)
+training1 = MyTorchClassifierTrainer(model=WineClassifer_torch_model,
+                                     dataloader=train_loader,
+                                     num_epochs=500,
+                                     loss_function=categorical_crossentropy)
 
-# Training mode
-num_epochs = 500
 start_time = time.time()
-
-for epoch in range(num_epochs):
-    WineClassifer_torch_model.train()
-    epoch_loss = 0.0
-    for batch_x, batch_y in train_loader:
-        batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-        optimizer.zero_grad()
-        predictions = WineClassifer_torch_model(batch_x)
-        loss = categorical_crossentropy(predictions, batch_y)
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-    avg_loss = epoch_loss / len(train_loader)
-    # print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}")
-
+training1.fit(verbose=True)
 print("PyTorch training took {:.2f} seconds".format(time.time() - start_time))
 
-# Eval mode:
-
-WineClassifer_torch_model.eval()
-with torch.no_grad():
-    test_outputs = WineClassifer_torch_model(X_test_tensor.to(device))
-    # Get predicted classes by taking argmax
-    torch_preds = test_outputs.argmax(dim=1).cpu().numpy()
-    print(f"predictions of y_test: {torch_preds} (large batches)")
-
-# Accuracy:
-print("WineClassifer_torch_model with larger batches: accuracy  = \t\t", accuracy_score(torch_preds, y_test),
-      " correctly cl. cases=", np.sum(torch_preds == y_test), " out of ", len(y_test))
+accuracy_and_predictions_model(WineClassifer_torch_model)
 
 # Model 2 - training with batch_size = 16
-batch_size2 = 16
-train_loader2 = DataLoader(train_dataset, batch_size=batch_size2, shuffle=True)
+
+train_loader2 = create_dataloader(train_dataset, batch_size=16)
 
 WineClassifer_torch_model2 = nn.Sequential(
     nn.Linear(13, 8),
@@ -105,38 +101,16 @@ WineClassifer_torch_model2 = nn.Sequential(
     nn.Softmax(dim=1)
 ).to(device)
 
-# Setup optimizer
-optimizer = optim.Adam(WineClassifer_torch_model2.parameters(), lr=0.01)
+print(" ")
+print("MODEL 2 (batch_size=16)")
 
-# Training mode
-num_epochs = 500
+training2 = MyTorchClassifierTrainer(model=WineClassifer_torch_model2,
+                                     dataloader=train_loader2,
+                                     num_epochs=500,
+                                     loss_function=categorical_crossentropy)
+
 start_time = time.time()
-
-for epoch in range(num_epochs):
-    WineClassifer_torch_model2.train()
-    epoch_loss = 0.0
-    for batch_x, batch_y in train_loader2:
-        batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-        optimizer.zero_grad()
-        predictions = WineClassifer_torch_model2(batch_x)
-        loss = categorical_crossentropy(predictions, batch_y)
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-    avg_loss = epoch_loss / len(train_loader2)
-    # print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}")
-
+training2.fit(verbose=True)
 print("PyTorch training took {:.2f} seconds".format(time.time() - start_time))
 
-# Eval mode:
-
-WineClassifer_torch_model2.eval()
-with torch.no_grad():
-    test_outputs = WineClassifer_torch_model2(X_test_tensor.to(device))
-    # Get predicted classes by taking argmax
-    torch_preds = test_outputs.argmax(dim=1).cpu().numpy()
-    print(f"predictions of y_test: {torch_preds} (small batches)")
-
-# Accuracy:
-print("WineClassifer_torch_model with smaller batches: accuracy  = \t\t", accuracy_score(torch_preds, y_test),
-      " correctly cl. cases=", np.sum(torch_preds == y_test), " out of ", len(y_test))
+accuracy_and_predictions_model(WineClassifer_torch_model2)
